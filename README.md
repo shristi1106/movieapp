1833338ed44198b37870f643cde45362f9505f2e
Shristi Verma <shristiverma1311@gmail.com>
2025-07-07 13:04:24 +0530

    frontend & backend with stripe

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# movie
+=======
+# movieapp
+>>>>>>> 4bac32b8632ea196bb3a69ac77082e9963656f24
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/BookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import { stripeWebhooks } from './controllers/stripeWebhooks.js';
+
+const app = express();
+const port = 3000;
+
+// Stripe Webhooks Route
+app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+await connectDB();
+
+//API Routes
+app.get('/', (req, res)=> res.send('server is live'))
+app.use("/api/inngest", serve({ client: inngest, functions }))
+app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
+app.use('api/user', userRouter)
+
+app.listen(port, ()=> console.log(`server listening at http://localhost:${port}`));
