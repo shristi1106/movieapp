d7a1d9e35e8c529052cfcf98b34ec04f82bffd6b
Shristi Verma <shristiverma1311@gmail.com>
2025-07-08 10:41:37 +0530

    admin dashboard problem fixed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,16 @@
 import { clerkClient } from "@clerk/express";
 
 export const protectAdmin = async (req, res, next) => {
-  // try {
-  //   const { userId } = req.auth();
+  try {
+    const { userId } = req.auth();
 
-  //   const user = await clerkClient.users.getUser(userId);
+    const user = await clerkClient.users.getUser(userId);
 
-  //   if (user.privateMetadata.role !== "admin") {
-  //     return res.json({ success: false, message: "not authorized" });
-  //   }
-
+    if (user.privateMetadata.role !== "admin") {
+      return res.json({ success: false, message: "not authorized" });
+    }
     next();
-  // } catch (error) {
-  //   return res.json({ success: false, message: "not authorized" });
-  // }
+  } catch (error) {
+    return res.json({ success: false, message: "not authorized" });
+  }
 };
